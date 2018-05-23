9ecbe6ddc66006c611af77fa9ca20d949358c53f
Trevor <trevorbautista@gmail.com>
2018-05-23 21:39:06 +0000

    [ARTIX] Improved orientation script to include all Wacom devices

--- ARTIX/dotfiles/bin/orientation.sh
+++ ARTIX/dotfiles/bin/orientation.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
 STYLUS="Wacom ISDv4 E6 Pen stylus"
+FINGER="Wacom ISDv4 E6 Finger touch"
+ERASER="Wacom ISDv4 E6 Pen eraser"
 
 case "$1" in
 
-	"up") /usr/bin/xrandr -o 0 && /usr/bin/xsetwacom set "$STYLUS" Rotate none
+	"up") 
+		/usr/bin/xrandr -o 0 && \
+		/usr/bin/xsetwacom set "$STYLUS" Rotate none && \
+		/usr/bin/xsetwacom set "$FINGER" Rotate none && \
+		/usr/bin/xsetwacom set "$ERASER" Rotate none
 		;;
 
-	"left") /usr/bin/xrandr -o 1 && /usr/bin/xsetwacom set "$STYLUS" Rotate ccw
+	"left") 
+		/usr/bin/xrandr -o 1 && \
+		/usr/bin/xsetwacom set "$STYLUS" Rotate ccw && \
+		/usr/bin/xsetwacom set "$FINGER" Rotate ccw && \
+		/usr/bin/xsetwacom set "$ERASER" Rotate ccw
 		;;
 
-	"right") /usr/bin/xrandr -o 3 && /usr/bin/xsetwacom set "$STYLUS" Rotate cw
+	"right") 
+		/usr/bin/xrandr -o 3 && \
+		/usr/bin/xsetwacom set "$STYLUS" Rotate cw && \
+		/usr/bin/xsetwacom set "$FINGER" Rotate cw && \
+		/usr/bin/xsetwacom set "$ERASER" Rotate cw
 		;;
 
-	"down") /usr/bin/xrandr -o 2 && /usr/bin/xsetwacom set "$STYLUS" Rotate half
+	"down") 
+		/usr/bin/xrandr -o 2 && \
+		/usr/bin/xsetwacom set "$STYLUS" Rotate half && \
+		/usr/bin/xsetwacom set "$FINGER" Rotate half && \
+		/usr/bin/xsetwacom set "$ERASER" Rotate half
 esac
